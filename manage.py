1c72968616b9d8842c192d49dbf5ea03b0f7aa09
Jake Malley <jja.malley@gmail.com>
2015-10-27 08:25:21 +0000

    Created Database Models and Migration Support
    Created the following database models: Student for storing information
    about the students, Parent for storing information about the parents,
    Tutor for storing information about the peripatetic tutors, Lesson for
    storing information about the peripatetic lessons, Room for storing
    information about the rooms and Instrument for storing information about
    the school instruments. StudentLessonAssociation and
    LessonTutorAssociation are used to create the many to many relationship
    between Student and Lesson as well as Lesson and Tutor respectively.
    
    Flask-Migrate was also added to allow easy upgrading of the database
    with any future changes. The Migrate command has be added to the Manager
    object and is available via manage.py.

--- manage.py
+++ manage.py
@@ -5,11 +5,17 @@
 # Flask Imports
 from flask.ext.script import Manager
 from flask.ext.script import Server
+from flask.ext.migrate import Migrate
+from flask.ext.migrate import MigrateCommand
 # Application Imports
 from peri2organise import app
+from peri2organise import db
+from peri2organise.models import Student, Parent, Tutor, Lesson, Room, Instrument, StudentLessonAssociation, LessonTutorAssociation
 
 # Create a Manager object.
 manager = Manager(app)
+# Create a Migration object.
+migrate = Migrate(app,db)
 
 # Check to see if configuration has been set for development server.
 try:
@@ -26,6 +32,19 @@ server = Server(host=DEVELOPMENT_SERVER_HOST, port=DEVELOPMENT_SERVER_PORT)
 
 # Add command for running the server.
 manager.add_command('runserver', server)
+# Add migrate command to the manager.
+manager.add_command('db', MigrateCommand)
+
+# Command to create the database.
+@manager.command
+def create_db():
+    """
+    Command to create the database.
+
+    Use this command to create the all the tables from the models in models.py.
+    (Alternatively Flask-Migrate can be used.)
+    """
+    db.create_all()
 
 if __name__ == '__main__':
     # Run the manager.
