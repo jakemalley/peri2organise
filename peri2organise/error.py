ee43108d90c84958e6e7624b5fced6d1d69f73b4
Jake Malley <jja.malley@gmail.com>
2016-02-11 16:08:20 +0000

    Updated Error Templates To Allow Users To Go Back

--- peri2organise/error.py
+++ peri2organise/error.py
@@ -4,13 +4,33 @@
 
 # Flask Imports
 from flask import render_template
+from flask import request
+from flask import url_for
 # Application Imports
 from peri2organise import app
+# Imports
+from urlparse import urlparse
+from urlparse import urljoin
+
+# Securely redirecting back - based on http://flask.pocoo.org/snippets/62/
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
+
+def get_redirect_target():
+    for target in request.referrer, url_for('home.index'):
+        if not target:
+            continue
+        if is_safe_url(target):
+            return target
 
 # Create a error handler for HTTP errors.
 def http_error_handler(error):
-    return render_template("error/error.html", error=error)
+    return render_template("error/error.html", error=error, redirect_back=get_redirect_target())
 
 # Apply this handler for specific HTTP error codes
 for error in (400, 401, 403, 404, 405, 500, 502):
     app.error_handler_spec[None][error] = http_error_handler
+
